4eaae6ae71a849fa5570de869ac211fa77ef809e
Matthew Turnshek <matthewturnshek@gmail.com>
2019-06-13 22:59:21 +0000

    complete basic cli summary generation

--- generate_summaries.py
+++ generate_summaries.py
@@ -6,6 +6,19 @@ gpt2.load_gpt2(sess)
 
 while True:
     title = input('title > ')
-    generations = gpt2.generate(sess, prefix='---###<title>###---' + title + '---###<summary>###---', include_prefix=True, nsamples=5, truncate="---###<end>###---", return_as_list=True)
+    generations = gpt2.generate(
+        sess,
+        prefix='---###<title>###---' + title + '---###<summary>###---',
+        include_prefix=True,
+        nsamples=16,
+        batch_size=16,
+        truncate="---###<end>###---",
+        temperature=0.75,
+        return_as_list=True
+    )
     for generation in generations:
-        print(generation)
+        summary = generation.split("-###<title>###---" + title + "---###<summary>###---")[1]
+        summary = summary.split("---###<end>###---")[0]
+        print(title, '\n')
+        print(summary, '\n')
+        print('\n\n\n')
